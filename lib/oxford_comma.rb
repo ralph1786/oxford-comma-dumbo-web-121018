c503872903af43e2d1060ce3430be27a25015d67
Rafael Cruz <ralph1786@gmail.com>
2018-11-20 23:33:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 2
+   return array.join(" and ")
+  elsif array.length == 1
+   return array.first()
+  else
+    return array[0..-2].join(', ') + ", and " + array.last()
+  end
 end
