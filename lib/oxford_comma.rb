270d0f1b5489e68315c4b5337e595250a3b0e213
Rafael Cruz <ralph1786@gmail.com>
2018-11-20 23:30:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
   if array.length == 2
    return array.join(" and ")
-   elsif array.length == 1
+  elsif array.length == 1
    return array.first()
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
